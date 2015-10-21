4a6ce3186bdfb0aab1d81c1e996858a87cedddf1
DannyEspina <DanielEspina@Dannys-MacBook-Pro.local>
2015-10-21 12:50:03 -0400

    Make a view profit page

--- Bikes/app/src/main/java/com/example/jaimejahuey/bikes/SalesManagement.java
+++ Bikes/app/src/main/java/com/example/jaimejahuey/bikes/SalesManagement.java
@@ -3,20 +3,33 @@ package com.example.jaimejahuey.bikes;
 /**
  * Created by jaimejahuey on 9/18/15.
  */
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 public class SalesManagement extends ActionBarActivity
 {
+    private Button ViewProfitButton;
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.sales_layout);
 
+        ViewProfitButton = (Button) findViewById(R.id.ViewProfit);
 
+        ViewProfitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(SalesManagement.this, viewProfit.class);
+
+                startActivity(i);
+            }
+        });
     }
 
     @Override
