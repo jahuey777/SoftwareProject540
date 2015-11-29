5b22406adfdf02cf57036128bd85d593e356ac21
jahuey777 <jahuey777@hotmail.com>
2015-11-29 18:53:36 -0500

    last changes

--- Bikes/app/src/main/java/com/example/jaimejahuey/bikes/profitSummary.java
+++ Bikes/app/src/main/java/com/example/jaimejahuey/bikes/profitSummary.java
@@ -13,6 +13,8 @@ import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.text.NumberFormat;
+
 /**
  * Created by jaimejahuey on 11/16/15.
  */
@@ -39,23 +41,25 @@ public class profitSummary extends Activity
 
         Log.w("Amount ", " " + information[0]);
 
+        NumberFormat numFormat = NumberFormat.getCurrencyInstance();
+
         totalProfit = (TextView)findViewById(R.id.ProfitAmount);
-        totalProfit.setText("" + information[0]);
+        totalProfit.setText("" + numFormat.format(information[0]));
 
         totalBikeProfitJ = (TextView)findViewById(R.id.TotalBikeProfit);
-        totalBikeProfitJ.setText("" + information[1]*.2);
+        totalBikeProfitJ.setText("" + numFormat.format(information[1]*.2));
 
         spentRepairsJ = (TextView)findViewById(R.id.SPENTREPAIRS);
-        spentRepairsJ.setText("" + information[2]);
+        spentRepairsJ.setText("" + numFormat.format(information[2]));
 
         chargedRepairsJ = (TextView)findViewById(R.id.CHARGEDREPAIRS);
-        chargedRepairsJ.setText("" + information[3]);
+        chargedRepairsJ.setText("" + numFormat.format(information[3]));
 
         totalBikeAmountJ = (TextView)findViewById(R.id.TOTALBIKEAMOUNT);
-        totalBikeAmountJ.setText("" + information[1]);
+        totalBikeAmountJ.setText("" + numFormat.format(information[1]));
 
         repairsProfitJ = (TextView)findViewById(R.id.RepairsProfit);
-        repairsProfitJ.setText("" + (information[3] - information[2]));
+        repairsProfitJ.setText("" + numFormat.format(information[3] - information[2]));
 
     }
 
